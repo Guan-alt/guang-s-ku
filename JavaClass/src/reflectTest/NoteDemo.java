1c0ec841d4c306ae8d7af3663c4234a92a7ec745
gw_ide <m17679409044@163.com>
2025-05-23 16:24:06 +0800

    关于untitled文件本地消失的提交

---
+++ JavaClass/src/reflectTest/NoteDemo.java
@@ -0,0 +1,37 @@
+package reflectTest;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * ClassName: NoteDemo
+ * Package: reflectTest
+ * Dscription:自定义注解，用于替换配置文件内容
+ *
+ * @Author: gw
+ * @Creat: 2025/5/23 - 9:28
+ */
+
+/**
+ * 1.@Target
+ *作用：指定自定义注解可以应用的目标元素类型（如类、方法、字段等）。
+ *属性：value（ElementType[] 数组）。
+ * 2.@Retention
+ * 作用：定义注解的保留策略，决定注解在何时有效（源码、类文件、运行时）。
+ * 属性：value（RetentionPolicy 枚举）。
+ * 3.@Documented
+ * 作用：标记注解是否应包含在生成的 JavaDoc 中。
+ * 4.@Inherited
+ * 作用：允许子类继承父类上的注解（仅对类级别的注解有效）。
+ * 5.@Repeatable（Java 8+）
+ * 作用：允许同一注解在同一个元素上多次使用。
+ * 属性：value（指定容器注解的类）。
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoteDemo {
+    String className();
+    String[] methodName();
+}
---
+++ JavaClass/src/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: test
+ * Package: test
+ * Dscription:
+ *
+ * @Author: gw
+ * @Creat: 2025/5/20 - 20:12
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println(124/10);
+        System.out.println(123%10);
+    }
+}
